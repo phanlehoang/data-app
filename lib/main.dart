4ced650203da4b8c57c06e0aedd28e380c9e4c16
phanlehoang <phanlehoang_t64@hus.edu.vn>
2023-01-08 11:32:33 +0700

    OK

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+//create a simple app
+import 'package:data_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
